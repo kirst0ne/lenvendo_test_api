b4f29bcdb149454745b5038aff2e73d0e130d2ea
Kirill Seleznev <okolo66@yandex.ru>
2024-11-11 21:52:34 +0300

    Сделаны доработки в рамках перехода по параметру 'page'

--- test/conftest.py
+++ test/conftest.py
@@ -6,12 +6,14 @@ import requests
 def api_client():
     base_url = "https://www.lenvendo.ru/api/js-test-task/"
 
-    def get_js_test_task(search=None, sort_field=None):
+    def get_js_test_task(search=None, sort_field=None, page=None):
         params = {}
         if search:
             params['search'] = search
         if sort_field:
             params['sort_field'] = sort_field
+        if page:
+            params['page'] = page
         response = requests.get(base_url, params=params)
         response.raise_for_status()
         return response.json()
--- test/test_api.py
+++ test/test_api.py
@@ -3,14 +3,22 @@ from fixture.JsTestTask import JsTestTask
 
 
 def test_js_test_task(api_client):
-    response_data = api_client(search="Alcatel", sort_field="name")
-    print("Response Data:")
-    pprint(response_data)
-    assert 'products' in response_data
-    products = response_data['products']
-    assert isinstance(products, list)
-    tasks = [JsTestTask(item['name'], item['image'], item['price']) for item in products]
-    for task in tasks:
-        assert "Alcatel" in task.name, f"Имя '{task.name}' не содержит 'Alcatel'"
-    sorted_names = sorted(task.name for task in tasks)
-    assert [task.name for task in tasks] == sorted_names, "Элементы не отсортированы по имени"
+    search_term = "Alcatel"
+    sort_field_term = "name"
+    current_page = 1
+    while True:
+        response_data = api_client(search=search_term, sort_field=sort_field_term, page=current_page)
+        print(f"Response Data for page {current_page}:")
+        pprint(response_data)
+        assert 'products' in response_data, "Ответ API содержит ключ 'products'"
+        products = response_data['products']
+        assert isinstance(products, list), "Ответ API содержит список продуктов"
+        tasks = [JsTestTask(item['name'], item['image'], item['price']) for item in products]
+        for task in tasks:
+            assert "Alcatel" in task.name, f"Имя '{task.name}' не содержит 'Alcatel'"
+        sorted_names = sorted(task.name for task in tasks)
+        assert [task.name for task in tasks] == sorted_names, "Элементы не отсортированы по имени"
+        if 'next_page_url' in response_data and response_data['next_page_url']:
+            current_page += 1
+        else:
+            break
